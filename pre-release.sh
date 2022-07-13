f92b07348a0a379ae1d37aa6496094ba91787a9f
Eli Kogan-Wang <elikowa@gmail.com>
2022-07-13 09:53:31 +0200

    Add tests,clippy,fmt to pre-release.sh

--- pre-release.sh
+++ pre-release.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 # generate example.config.yaml with ./generate_config_example.py
 
 ./generate_config_example.py > example.config.yaml
+
+# also run tests, clippy and fmt
+
+cargo test --all --locked
+
+cargo clippy
+
+cargo fmt --all -- --check
